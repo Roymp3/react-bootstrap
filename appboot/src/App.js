51f93ef84a8b872fbc1fb04954fd4c7e95562277
Roymp3 <71888012+Roymp3@users.noreply.github.com>
2021-04-05 20:05:08 -0300

    Add files via upload

---
+++ appboot/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Jumbotron } from 'react-bootstrap';
+ 
+function App() {
+  return (
+    <Jumbotron>
+      <h1>Hello, world!</h1>
+      <p>
+        Primeiro exemplo de utilização do React-Bootstrap, nosso Olá Mundo.
+      </p>
+    </Jumbotron>
+  );
+}
+ 
+export default App;
